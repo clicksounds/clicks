a2b68d1b6686986d077298d0ad37cfbb2e08c3fe
Cooper <73203995+coopeeo@users.noreply.github.com>
2024-03-25 21:45:06 -0500

    gfdsgfdsgfdsgfdsgfdsgfds

--- .github/CopySoundsForRenamePT2.py
+++ .github/CopySoundsForRenamePT2.py
@@ -1,8 +1,8 @@
 import shutil
 import os
 #shutil.rmtree("Meme")
-shutil.copytree(".github/FileNamer/Output/Meme", "Meme/")
+shutil.copytree(".github/FileNamer/Output/Meme", "Meme")
 #shutil.rmtree("Useful")
-shutil.copytree(".github/FileNamer/Output/Useful", "Useful/")
+shutil.copytree(".github/FileNamer/Output/Useful", "Useful")
 shutil.rmtree(".github/FileNamer/Output")
 shutil.rmtree(".github/FileNamer/Update")
