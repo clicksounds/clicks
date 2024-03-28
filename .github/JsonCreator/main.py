89b497153e8ad2d62aac34a665a31926aab96dcd
Cooper <73203995+coopeeo@users.noreply.github.com>
2024-03-28 16:19:47 -0500

    do this

--- .github/JsonCreator/main.py
+++ .github/JsonCreator/main.py
@@ -5,41 +5,10 @@ import json
 import time
 
 jsonshit = {
-    "Clicks": {
-        "Meme": [],
-        "Useful": []
-    },
-    "Releases": {
-        "Meme": [],
-        "Useful": []
-    }
+    "everything": []
 }
-jsonshit2 = {
-    "Clicks": {
-        "Meme": {},
-        "Useful": {}
-    },
-    "Releases": {
-        "Meme": {},
-        "Useful": {}
-    }
-}
-jsonshit3 = {
-    "CM": 0,
-    "CU": 0,
-    "RM": 0,
-    "RU": 0
-}
-jsonshitForChecking = {
-    "Clicks": {
-        "Meme": [],
-        "Useful": []
-    },
-    "Releases": {
-        "Meme": [],
-        "Useful": []
-    }
-}
+filecrap = json.reads(os.environ['ALL_FILES'])
+
 
 def rename_files():
     folder_path = shutil.copytree("Update", "Output")
@@ -51,24 +20,20 @@ def rename_files():
             print(root)
             name = root.split("/")[2]
             memeUseful = root.split("/")[1]
-            clicksRelease = root.split("/")[3]
             # split whole file name to just the start and the extension
             filename, file_extension = os.path.splitext(file)
             # check if click name exists in json
-            if name not in jsonshitForChecking[clicksRelease][memeUseful]:
-                jsonshitForChecking[clicksRelease][memeUseful].append(name)
-                jsonshit[clicksRelease][memeUseful].append({
-                    "name": name.replace("_", " "),
-                    "prefix": name,
-                    "files": [filename],
-                    "fileCount": 1
-                })
-            else:
-                for o in jsonshit[clicksRelease][memeUseful]:
-                    if o["name"] == name:
-                        o["files"].append(filename)
-                        o["fileCount"] += 1
-                        jsonshit
+            if file == "pack.json":
+                pack = {}
+                with open(root + file, "r") as file:
+                    lines = file.readlines()
+                    pack = json.reads('\n'.join(lines))
+                pack["click-files"] = filecrap[name]["c"]
+                pack["release-files"] = filecrap[name]["v"]
+                with open(root + file, "w") as file:
+                    for line in [json.dumps(pack)]:
+                        file.write(f'{line}\n')
+                jsonshit["everything"].append(pack)
 
 if __name__ == "__main__":
     if os.path.exists("Output"):
@@ -79,7 +44,7 @@ if __name__ == "__main__":
 
     rename_files()
 
-    jsonshitall = {"Reg":jsonshit,"Back":jsonshit2, "Len": jsonshit3}
+    jsonshitall = jsonshit["everything"]
 
     with open("../../list.json", "w") as file:
         for line in [json.dumps(jsonshitall)]:
